632e7b6051980c36ff6e8f3f2748c11f3817909c
khi <llioopv@naver.com>
2025-09-07 13:10:15 +0900

    feat: Add totp-service

--- security-service/src/main/java/com/khi/securityservice/core/entity/domain/ProductSecretEntity.java
+++ security-service/src/main/java/com/khi/securityservice/core/entity/domain/ProductSecretEntity.java
@@ -1,8 +1,6 @@
 package com.khi.securityservice.core.entity.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,10 +10,12 @@ import lombok.Setter;
 public class ProductSecretEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String clientId;
+    private String clientSecret;
 
     @OneToOne
     private ProductEntity product;
-
-    private String clientSecret;
 }
