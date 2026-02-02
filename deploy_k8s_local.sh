b8c0afc75d2dd9e5d897c9b7a4ebd0eab0c3bbce
khi <llioopv@naver.com>
2026-02-02 18:03:24 +0900

    refactor: update k8s files hierarchy

--- deploy_k8s_local.sh
+++ deploy_k8s_local.sh
@@ -4,25 +4,20 @@ set -e # 에러 발생 시 스크립트 즉시 종료
 
 echo "로컬 쿠버네티스 배포 시작"
 
-# 1. 인프라 배포
-echo "인프라 배포 시작"
+# 1. 인프라/모니터링 배포
+echo "인프라/모니터링 배포 시작"
 kubectl apply -f k8s/infra/
+kubectl apply -f k8s/monitoring/
 
-# 2. 서비스 배포
-echo "서비스 배포 시작"
+# 2. 서비스 빌드
+echo "서비스 빌드 시작"
 services=("apigateway-service" "security-service" "onboarding-service" "totp-service")
-
 for service in "${services[@]}"; do
-    
-    echo "서비스 배포중... $service"
-    
     docker build -t $service:latest --build-arg SERVICE_NAME=$service .
-    
-    kubectl apply -f $service/k8s/depl-svc.yml
-    
-    # HPA 배포
-    echo "HPA 배포: $service"
-    kubectl apply -f $service/k8s/hpa.yml
 done
+
+# 3. 서비스 배포
+echo "서비스 배포 시작"
+kubectl apply -f k8s/service/
 
 echo "배포 완료"
