f4d2d4fc8082b21f59e8245b7ab60d8e8fcada56
khi <llioopv@naver.com>
2026-02-04 11:54:24 +0900

    feat: add docker hub push logic in local k8s

--- k8s/scripts/init_deploy_k8s_local.sh
+++ k8s/scripts/init_deploy_k8s_local.sh
@@ -4,15 +4,23 @@ set -e
 
 echo "로컬 쿠버네티스 초기 배포 시작"
 
+
+# 1. Docker Hub 로그인
+./k8s/scripts/docker_login.sh
+
+# 2. 네임스페이스 생성
 echo "네임스페이스 생성중..."
 kubectl apply -f k8s/namespace/service_namespace.yml
 
+# 3. 시크릿 생성
 echo "시크릿 생성중..."
 ./k8s/secret/create_secret.sh
 
+# 4. 인프라/모니터링 배포
 kubectl apply -f k8s/infra/
 kubectl apply -f k8s/monitoring/
 
+# 5. Redis (Sentinel) 설치
 echo "Redis (Sentinel) 설치중..."
 ./helm repo add bitnami https://charts.bitnami.com/bitnami
 ./helm repo update
@@ -24,12 +32,16 @@ echo "Redis (Sentinel) 설치중..."
   --set image.tag=7.2 \
   --wait
 
+# 6. 서비스 빌드
 echo "서비스 빌드중..."
 services=("apigateway-service" "security-service" "onboarding-service" "totp-service")
 for service in "${services[@]}"; do
-    docker build -t $service:latest --build-arg SERVICE_NAME=$service .
+    IMAGE_NAME="kwonhyeongil/$service:latest"
+    docker build -t $IMAGE_NAME --build-arg SERVICE_NAME=$service .
+    docker push $IMAGE_NAME
 done
 
+# 7. 서비스 배포
 echo "서비스 배포중..."
 kubectl apply -R -f k8s/service/ -n service
 
--- k8s/scripts/redeploy_k8s_local.sh
+++ k8s/scripts/redeploy_k8s_local.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-echo "변경된 서비스만 재배포 시작"
 
+# 1. Docker Hub 로그인
+./k8s/scripts/docker_login.sh
+
+# 2. 변경된 서비스 탐색
+echo "변경된 서비스만 재배포 시작"
 ALL_SERVICES=("apigateway-service" "security-service" "onboarding-service" "totp-service")
 TARGET_SERVICES=()
-
 echo "변경된 서비스 검색 중..."
 for service in "${ALL_SERVICES[@]}"; do
     if [ -n "$(git status --porcelain "$service")" ]; then
@@ -20,12 +23,13 @@ if [ ${#TARGET_SERVICES[@]} -eq 0 ]; then
     exit 0
 fi
 
+# 3. 변경된 서비스 빌드 및 배포
 echo "배포 대상: ${TARGET_SERVICES[*]}"
-
 for service in "${TARGET_SERVICES[@]}"; do
-
     echo "[$service] 재배포 진행 중..."
-    docker build -t $service:latest --build-arg SERVICE_NAME=$service .
+    IMAGE_NAME="kwonhyeongil/$service:latest"
+    docker build -t $IMAGE_NAME --build-arg SERVICE_NAME=$service .
+    docker push $IMAGE_NAME
     kubectl rollout restart deployment ${service}-depl -n service
     echo "[$service] 완료"
 done
